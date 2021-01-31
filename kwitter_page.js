2ef5d1a36f2231138157ffaf93cb64e0292d15ca
aisha100708 <71584539+aisha100708@users.noreply.github.com>
2021-01-31 12:50:16 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -27,14 +27,39 @@ function send() {
       document.getElementById("type_msg").value = "";
 }
 
-function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+function getData() {
+      firebase.database().ref("/"+room_name).on('value', function(snapshot) { 
+      document.getElementById("output").innerHTML = "";
+      snapshot.forEach(function(childSnapshot) {
+            childKey  = childSnapshot.key;
+            childData = childSnapshot.val();
+            if(childKey != "purpose") {
          firebase_message_id = childKey;
          message_data = childData;
 //Start code
-
+console.log(firebase_message_id);
+console.log(message_data);
+message = message_data ['message'];
+username = message_data ['name'];
+likes = message_data ['like'];
+name_tag = "<h3>" + username + "<img src='tick.png' class='user_tick'> </h3>";
+msg_tag = "<h4 class = 'message_h4'>" + message + "</h4>";
+like_tag = "<button class='btn btn-warning' id='"+firebase_message_id+"' value='"+likes+"' onclick='updateLikes(this.id)'>";
+span_with_like_tag = "<span class='glyphicon glyphicon-thumbs-up'> like: " + likes + "</span> </button>";
+row = name_tag + msg_tag + like_tag + span_with_like_tag;
+document.getElementById("output").innerHTML += row;
 //End code
       } });  }); }
 getData();
+
+function updateLikes(message_id) {
+      console.log(message_id);
+      new_likes = document.getElementById(message_id).value;
+      no_of_likes = Number(new_likes) + 1;
+      firebase.database().ref(room_name).child(message_id).update({
+            like: no_of_likes
+      });
+}
 
 function logOut() {
       localStorage.removeItem("username_key");
